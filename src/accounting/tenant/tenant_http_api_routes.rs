bee652e59ceb3e13b0d3a6ef4064fab502bd3da9
Aayushya Vajpayee <aayushyavajpayee@gmail.com>
2023-08-19 12:20:17 +0530

    use id to fetch tenant

--- src/accounting/tenant/tenant_http_api_routes.rs
+++ src/accounting/tenant/tenant_http_api_routes.rs
@@ -1,8 +1,8 @@
 use actix_web::{Responder, web};
 use crate::accounting::tenant::tenant_service::{get_tenant_service, TenantService};
 
-async fn get_tenant_by_id(mut data: web::Data<Box<dyn TenantService + Send + Sync>>) -> actix_web::Result<impl Responder> {
-    let t = data.get_tenant_by_id(&1).await;
+async fn get_tenant_by_id(id: web::Path<i32>, data: web::Data<Box<dyn TenantService + Send + Sync>>) -> actix_web::Result<impl Responder> {
+    let t = data.get_tenant_by_id(&id).await;
     Ok(web::Json(t))
 }
 
@@ -12,6 +12,6 @@ pub fn init_routes(config: &mut web::ServiceConfig) {
     let data = web::Data::new(tenant_service);
     config.service(web::scope("/tenant")
         .app_data(data)
-        .route("/id", web::get().to(get_tenant_by_id)))
+        .route("/id/{id}", web::get().to(get_tenant_by_id)))
     ;
 }
