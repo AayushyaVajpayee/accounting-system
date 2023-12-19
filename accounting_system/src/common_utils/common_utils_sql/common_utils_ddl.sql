71516bc5aa0f1b1de678e4534578d6c70de2d849
Aayushya Vajpayee <aayushyavajpayee@gmail.com>
2023-12-19 13:17:02 +0530

    fixes ACC-162 address seed and ddl not in sync with entity struct

--- accounting_system/src/common_utils/common_utils_sql/common_utils_ddl.sql
+++ accounting_system/src/common_utils/common_utils_sql/common_utils_ddl.sql
@@ -1,5 +1,6 @@
 create type mime_type as enum ('csv','docx','jpeg','json','png','pdf','txt','xlsx');
-create type workflow_type as enum ('dummy_test','create_tenant','create_account_type_mst','create_account','create_currency','create_app_user','create_company_mst');
+create type workflow_type as enum ('dummy_test','create_tenant','create_account_type_mst','create_account',
+    'create_currency','create_app_user','create_company_mst','create_address');
 create table idempotence_store
 (
     idempotence_key uuid          not null,
---
+++ accounting_system/src/masters/address_master/address_sql/address_functions_and_procedures.sql
@@ -0,0 +1,33 @@
+--
+--
+--
+-- create type create_address_request as
+-- (
+--     idempotence_key uuid,
+--     tenant_id uuid,
+--     line_1 text,
+--     line_2 text,
+--     landmark text,
+--     city_id uuid,
+--     state_id uuid,
+--     country_id uuid
+-- );
+-- create or replace function create_address(req create_address_request) returns uuid as
+-- $$
+-- DECLARE
+--     resp jsonb;
+--     address_id uuid;
+--     impacted_rows int;
+-- BEGIN
+--     insert into idempotence_store  (idempotence_key, workflow_type, response, created_at, updated_at)
+--     values (req.idempotence_key,'create_address',null,default,default) on conflict do nothing;
+--     get diagnostics impacted_rows = row_count;
+--     if impacted_rows!=0 then
+--         select uuid_generate_v7() into address_id;
+--         insert into address (id, entity_version_id, tenant_id, active, approval_status, remarks, pincode_id,
+--                              city_id, state_id, country, line_1, line_2, landmark, created_by, updated_by,
+--                              created_at, updated_at)
+--         values (address_id,)
+--     end if;
+-- end
+-- $$ language plpgsql;
