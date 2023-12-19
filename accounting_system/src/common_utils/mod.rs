61c2156bb4e3dd0cecc7589f92816796befac120
Aayushya Vajpayee <aayushyavajpayee@gmail.com>
2023-12-19 09:58:30 +0530

    adding mods

--- accounting_system/src/common_utils/mod.rs
+++ accounting_system/src/common_utils/mod.rs
@@ -3,3 +3,4 @@ pub mod mime_types;
 pub mod dao_error;
 pub mod pagination;
 pub mod macro_utils;
+pub mod db_row_conversion_utils;
--- accounting_system/src/masters/address_master/mod.rs
+++ accounting_system/src/masters/address_master/mod.rs
@@ -1,3 +1,4 @@
 mod address_model;
 mod address_dao;
 mod address_service;
+mod address_http_api;
