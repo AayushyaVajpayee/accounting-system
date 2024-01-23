6c48fc5086e64a7f6657d8292240f2c3415ef2f6
Aayushya Vajpayee <aayushyavajpayee@gmail.com>
2024-01-23 17:07:06 +0530

    rectify temporal configuration. this works in kubernetes

--- accounting_temporal_worker/src/main/kotlin/com/temporal/accounting/TemporalConfiguration.kt
+++ accounting_temporal_worker/src/main/kotlin/com/temporal/accounting/TemporalConfiguration.kt
@@ -1,17 +1,23 @@
 package com.temporal.accounting
 
 import com.temporal.accounting.workflows.invoicing.InvoiceCreationActivitiesImpl
-import com.temporal.accounting.workflows.invoicing.InvoiceCreationWorkflow
 import com.temporal.accounting.workflows.invoicing.InvoiceCreationWorkflowImpl
+import io.grpc.ManagedChannelBuilder
 import io.temporal.client.WorkflowClient
-import io.temporal.client.WorkflowOptions
 import io.temporal.serviceclient.WorkflowServiceStubs
+import io.temporal.serviceclient.WorkflowServiceStubsOptions
 import io.temporal.worker.WorkerFactory
+import java.time.Duration
 
 object TemporalConfiguration {
 
     val service by lazy{
-        WorkflowServiceStubs.newLocalServiceStubs()
+        WorkflowServiceStubs.newConnectedServiceStubs(WorkflowServiceStubsOptions.newBuilder()
+            .setChannel(ManagedChannelBuilder
+                .forAddress("accounting-temporal-frontend",7233)//todo take the service name as env variable through kubernetes instead of hardcoding here.
+                .usePlaintext().build())
+
+            .build(), Duration.ofSeconds(30))
     }
 
     val client by lazy{
