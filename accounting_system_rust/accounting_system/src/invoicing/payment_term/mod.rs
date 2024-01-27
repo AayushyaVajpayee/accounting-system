f686021abce0e1d58041b068a5c0c3aa7bd2de66
Aayushya Vajpayee <aayushyavajpayee@gmail.com>
2024-01-27 19:29:53 +0530

    fixes ACC-201 add unit test for get_or_create_payment_term

--- accounting_system_rust/accounting_system/src/invoicing/payment_term/mod.rs
+++ accounting_system_rust/accounting_system/src/invoicing/payment_term/mod.rs
@@ -1,3 +1,3 @@
 pub mod payment_term_db_mapping;
-// mod payment_term_dao;
-// pub mod payment_term_models;
+mod payment_term_dao;
+pub mod payment_term_models;
---
+++ accounting_system_rust/accounting_system/src/invoicing/payment_term/payment_term_dao.rs
@@ -0,0 +1,67 @@
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+    use actix_web::web::get;
+    use rstest::rstest;
+    use spectral::assert_that;
+    use spectral::option::OptionAssertions;
+    use tokio_postgres::SimpleQueryMessage;
+    use uuid::Uuid;
+    use crate::accounting::postgres_factory::test_utils_postgres::{get_postgres_conn_pool, get_postgres_image_port};
+    use crate::accounting::user::user_models::SEED_USER_ID;
+    use crate::invoicing::payment_term::payment_term_models::tests::SEED_PAYMENT_TERM_ID;
+    use crate::tenant::tenant_models::SEED_TENANT_ID;
+
+    #[tokio::test]
+    async fn should_retrieve_existing_get_or_create_payment_term() {
+        let port = get_postgres_image_port().await;
+        let postgres_client = get_postgres_conn_pool(port, None).await;
+        let query_form = format!(r#"
+            begin transaction;
+            select get_or_create_payment_term({},{},{},'{}','{}');
+            commit;
+        "#, 5, 0, 0, *SEED_TENANT_ID, *SEED_USER_ID);
+        let p = postgres_client.get().await.unwrap()
+            .simple_query(&query_form).await.unwrap();
+        let ak = p.get(1).unwrap();
+        match ak {
+            SimpleQueryMessage::Row(a) => {
+                let p: Option<&str> = a.get(0);
+                assert_that!(p).is_some()
+                    .matches(|a|**a == *SEED_PAYMENT_TERM_ID.to_string().as_str());
+            }
+            SimpleQueryMessage::CommandComplete(a) => {
+                unreachable!();
+            }
+            _ =>{unreachable!();}
+        }
+    }
+    #[tokio::test]
+    async fn should_create_new_get_or_create_payment_term() {
+        let port = get_postgres_image_port().await;
+        let postgres_client = get_postgres_conn_pool(port, None).await;
+        let query_form = format!(r#"
+            begin transaction;
+            select get_or_create_payment_term({},{},{},'{}','{}');
+            commit;
+        "#, 5, 02, 0, *SEED_TENANT_ID, *SEED_USER_ID);
+        let p = postgres_client.get().await.unwrap()
+            .simple_query(&query_form).await.unwrap();
+        let ak = p.get(1).unwrap();
+        match ak {
+            SimpleQueryMessage::Row(a) => {
+                let p: Option<&str> = a.get(0);
+                assert_that!(p).is_some()
+                    .matches(|a|**a != *SEED_PAYMENT_TERM_ID.to_string().as_str());
+                let id = Uuid::from_str(p.unwrap()).unwrap();
+                let q = postgres_client.get().await.unwrap().query_opt("select * from payment_term where id=$1",&[&id])
+                    .await.unwrap();
+                assert_that!(q).is_some();
+            }
+            SimpleQueryMessage::CommandComplete(a) => {
+                unreachable!();
+            }
+            _ =>{unreachable!();}
+        }
+    }
+}
