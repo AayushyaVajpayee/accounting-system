b8b3e43dc9e03eff16bae8015eedd7cd5ed1c4c7
Aayushya Vajpayee <aayushyavajpayee@gmail.com>
2024-01-28 18:34:27 +0530

    work in progress

--- accounting_system_rust/accounting_system/src/invoicing/additional_charge/additional_charge_dao.rs
+++ accounting_system_rust/accounting_system/src/invoicing/additional_charge/additional_charge_dao.rs
@@ -1,21 +1,35 @@
-
-
-
-
-
 #[cfg(test)]
-mod tests{
+mod tests {
     use spectral::assert_that;
     use tokio_postgres::SimpleQueryMessage;
     use xxhash_rust::xxh32;
     use crate::accounting::postgres_factory::test_utils_postgres::{get_postgres_conn_pool, get_postgres_image_port};
+    use crate::invoicing::additional_charge::additional_charge_models::CreateAdditionalChargeRequestDbModel;
+    use crate::invoicing::additional_charge::additional_charge_models::tests::a_create_additional_charge_request_db_model;
     use crate::tenant::tenant_models::SEED_TENANT_ID;
 
-    #[tokio::test]
-    async fn test(){
 
+    fn convert_to_db_add_charge_input(charges: &Vec<CreateAdditionalChargeRequestDbModel>) -> String {
+        let mut k: Vec<String> = Vec::new();
+        for x in charges {
+            let p = format!("('{}',{}::smallint,'{}',{},{})",x.line_id,x.line_no,x.line_title,x.title_xx_hash,x.rate);
+            k.push(p);
+        }
+        format!("array[{}]::create_additional_charge_request[]",k.join(","))
+    }
+
+    #[tokio::test]
+    async fn test() {
         let port = get_postgres_image_port().await;
         let postgres_client = get_postgres_conn_pool(port, None).await;
+        let charges = vec![
+            a_create_additional_charge_request_db_model(Default::default()),
+            a_create_additional_charge_request_db_model(Default::default())];
+        let query_form = format!(r#"
+        begin transaction;
+        select persist_additional_charge({},'{}','{}','{}');
+        commit;
+        "#,convert_to_db_add_charge_input(&charges),,*SEED_TENANT_ID,*SEED_USER_ID);
         // let mut hasher = xxh32::Xxh32::new(0);
         // hasher.update("line subtitle".as_bytes());
         // let xxhash = hasher.digest();
--- accounting_system_rust/accounting_system/src/invoicing/additional_charge/additional_charge_models.rs
+++ accounting_system_rust/accounting_system/src/invoicing/additional_charge/additional_charge_models.rs
@@ -1,22 +1,42 @@
+use derive_builder::Builder;
+use uuid::Uuid;
 
-
-
-
-
-
-
-
-
-
-
+#[derive(Builder, Debug)]
+pub struct CreateAdditionalChargeRequestDbModel {
+    pub line_id: Uuid,
+    pub line_no: i16,
+    pub line_title: String,
+    pub title_xx_hash: u32,
+    pub rate: f64,
+}
 
 
 #[cfg(test)]
-pub mod tests{
+pub mod tests {
     use std::str::FromStr;
+
     use lazy_static::lazy_static;
     use uuid::Uuid;
-    lazy_static!{
+    use xxhash_rust::xxh32;
+
+    use crate::invoicing::additional_charge::additional_charge_models::{CreateAdditionalChargeRequestDbModel, CreateAdditionalChargeRequestDbModelBuilder};
+
+    lazy_static! {
         pub static ref ADDITIONAL_CHARGE_SEED_ID:Uuid = Uuid::from_str("").unwrap();
+    }
+
+
+pub fn a_create_additional_charge_request_db_model(builder: CreateAdditionalChargeRequestDbModelBuilder) -> CreateAdditionalChargeRequestDbModel {
+        let line_title = builder.line_title.unwrap_or("additional charge".to_string());
+        let mut k = xxh32::Xxh32::new(0);
+        k.update(line_title.as_bytes());
+        let a = k.digest();
+        CreateAdditionalChargeRequestDbModel {
+            line_id: builder.line_id.unwrap_or_else(Uuid::now_v7),
+            line_no: builder.line_no.unwrap_or(1),
+            line_title,
+            title_xx_hash: builder.title_xx_hash.unwrap_or(a),
+            rate: builder.rate.unwrap_or(20.0),
+        }
     }
 }
