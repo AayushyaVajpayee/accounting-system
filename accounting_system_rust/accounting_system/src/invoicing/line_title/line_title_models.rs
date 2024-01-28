df09bde857aa7e49ffb268a18dbb17cbf00d5d30
Aayushya Vajpayee <aayushyavajpayee@gmail.com>
2024-01-28 13:31:17 +0530

    fixes ACC-205 add unit tests for line title

---
+++ accounting_system_rust/accounting_system/src/invoicing/line_title/line_title_models.rs
@@ -0,0 +1,14 @@
+
+
+
+
+
+#[cfg(test)]
+pub mod tests{
+    use std::str::FromStr;
+    use lazy_static::lazy_static;
+    use uuid::Uuid;
+    lazy_static!{
+        pub static ref SEED_LINE_TITLE_HSN_ID:Uuid = Uuid::from_str("018d4b77-e86b-77f0-ace4-02c34a72399b").unwrap();
+    }
+}
--- accounting_system_rust/accounting_system/src/invoicing/line_title/mod.rs
+++ accounting_system_rust/accounting_system/src/invoicing/line_title/mod.rs
@@ -1,1 +1,3 @@
 pub mod line_title_db_mapping;
+mod line_title_dao;
+mod line_title_models;
