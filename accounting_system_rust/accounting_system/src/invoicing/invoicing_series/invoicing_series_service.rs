555e6ec231f248e33be9cf69eadfc32f82c8987b
Aayushya Vajpayee <aayushyavajpayee@gmail.com>
2024-02-08 14:22:44 +0530

    fixes ACC-252 add cache to invoicing series

--- accounting_system_rust/accounting_system/src/invoicing/invoicing_series/invoicing_series_service.rs
+++ accounting_system_rust/accounting_system/src/invoicing/invoicing_series/invoicing_series_service.rs
@@ -2,12 +2,15 @@ use async_trait::async_trait;
 #[cfg(test)]
 use mockall::automock;
 use std::sync::Arc;
+use std::time::Duration;
+use deadpool_postgres::Pool;
 use moka::future::Cache;
 use thiserror::Error;
 use uuid::Uuid;
+use crate::common_utils::cache_utils::get_or_fetch_entity;
 
 use crate::common_utils::dao_error::DaoError;
-use crate::invoicing::invoicing_series::invoicing_series_dao::InvoicingSeriesDao;
+use crate::invoicing::invoicing_series::invoicing_series_dao::{get_invoicing_series_dao, InvoicingSeriesDao};
 use crate::invoicing::invoicing_series::invoicing_series_models::{CreateInvoiceNumberSeriesRequest, InvoicingSeriesMaster};
 
 #[derive(Debug, Error)]
@@ -21,7 +24,7 @@ pub enum InvoicingSeriesServiceError {
 pub trait InvoicingSeriesService: Send + Sync {
     async fn create_invoice_series(&self, request: &CreateInvoiceNumberSeriesRequest) -> Result<Uuid, InvoicingSeriesServiceError>;
 
-    async fn get_invoicing_series_by_id(&self, invoicing_series_id: Uuid,tenant_id:Uuid) -> Result<Option<InvoicingSeriesMaster>, InvoicingSeriesServiceError>;
+    async fn get_invoicing_series_by_id(&self, invoicing_series_id: Uuid,tenant_id:Uuid) -> Result<Option<Arc<InvoicingSeriesMaster>>, InvoicingSeriesServiceError>;
     async fn is_valid_invoicing_series_id(&self,invoicing_series_id:Uuid,tenant_id:Uuid)->Result<bool,InvoicingSeriesServiceError>;
 }
 
@@ -30,6 +33,18 @@ struct InvoicingSeriesServiceImpl {
     cache:Cache<(Uuid,Uuid),Arc<InvoicingSeriesMaster>>
 }
 
+async fn get_invoicing_series_service(pool:Arc<Pool>)->Arc<dyn InvoicingSeriesService>{
+    let invoicing_series_dao=get_invoicing_series_dao(pool);
+    let cache:Cache<(Uuid,Uuid),Arc<InvoicingSeriesMaster>> = Cache::builder()
+        .max_capacity(1000)
+        .time_to_live(Duration::from_secs(300))
+        .build();
+    let invoicing_series_service = InvoicingSeriesServiceImpl{
+        dao:invoicing_series_dao,
+        cache
+    };
+    Arc::new(invoicing_series_service)
+}
 
 #[async_trait]
 impl InvoicingSeriesService for InvoicingSeriesServiceImpl {
@@ -38,13 +53,19 @@ impl InvoicingSeriesService for InvoicingSeriesServiceImpl {
         Ok(d)
     }
 
-    async fn get_invoicing_series_by_id(&self, invoicing_series_id: Uuid,tenant_id:Uuid) -> Result<Option<InvoicingSeriesMaster>, InvoicingSeriesServiceError> {
-        let opt_in_ser = self.dao.get_invoicing_series_by_id(invoicing_series_id,tenant_id).await?;
-        Ok(opt_in_ser)
+    async fn get_invoicing_series_by_id(&self, invoicing_series_id: Uuid,tenant_id:Uuid) -> Result<Option<Arc<InvoicingSeriesMaster>>, InvoicingSeriesServiceError> {
+        let fetch = async{
+            let p =self.dao.get_invoicing_series_by_id(invoicing_series_id,tenant_id).await?;
+            Ok(p)
+        };
+        get_or_fetch_entity(tenant_id,invoicing_series_id,&self.cache,fetch).await
     }
 
     async fn is_valid_invoicing_series_id(&self, invoicing_series_id: Uuid, tenant_id: Uuid) -> Result<bool, InvoicingSeriesServiceError> {
-        todo!()
+        let p = self
+            .get_invoicing_series_by_id(invoicing_series_id,tenant_id)
+            .await?;
+        Ok(p.is_some())
     }
 }
 
