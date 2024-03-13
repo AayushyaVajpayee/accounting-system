9c80abde4469ab20b3661ee57333945355d0a5db
Aayushya Vajpayee <aayushyavajpayee@gmail.com>
2024-03-13 18:12:29 +0530

    fixes ACC-317 client should throw error if the response is non 2xx

--- accounting_temporal_worker/src/main/kotlin/com/temporal/accounting/Application.kt
+++ accounting_temporal_worker/src/main/kotlin/com/temporal/accounting/Application.kt
@@ -1,10 +1,18 @@
 package com.temporal.accounting
 
 import com.temporal.accounting.plugins.configureRouting
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.logging.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-
+val httpClient by lazy{
+    HttpClient(CIO){
+        expectSuccess = true
+        install(Logging)
+    }
+}
 fun main() {
     TemporalConfiguration.start()
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
