729c09a3e2a63031393ccc7fa11bfe140030398c
Aayushya Vajpayee <aayushyavajpayee@gmail.com>
2024-03-26 22:23:49 +0530

    adding seed to db schema syncer

--- accounting_system_rust/accounting_system/src/masters/mod.rs
+++ accounting_system_rust/accounting_system/src/masters/mod.rs
@@ -5,4 +5,4 @@ pub mod company_master;
 pub mod address_master;
 pub mod country_master;
 pub mod business_entity_master;
-mod product_item_master;
+pub mod product_item_master;
--- accounting_system_rust/accounting_system/src/masters/product_item_master/mod.rs
+++ accounting_system_rust/accounting_system/src/masters/product_item_master/mod.rs
@@ -2,4 +2,4 @@ mod product_item_models;
 mod product_item_dao;
 mod product_item_service;
 mod product_item_db_models;
-mod product_item_db_mapping;
+pub mod product_item_db_mapping;
