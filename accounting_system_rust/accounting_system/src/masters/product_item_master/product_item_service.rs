d16a8fb75d16378faa85c582ca49c08ce4c2e95d
Aayushya Vajpayee <aayushyavajpayee@gmail.com>
2024-04-01 18:30:52 +0530

    fixes ACC-357 implement batch get_product_items db call

--- accounting_system_rust/accounting_system/src/masters/product_item_master/product_item_service.rs
+++ accounting_system_rust/accounting_system/src/masters/product_item_master/product_item_service.rs
@@ -1,12 +1,16 @@
 use std::sync::Arc;
 use std::time::Duration;
+
+use actix_web::body::MessageBody;
 use async_trait::async_trait;
 use deadpool_postgres::Pool;
+use itertools::Itertools;
 #[cfg(test)]
 use mockall::automock;
 use moka::future::Cache;
 use thiserror::Error;
 use uuid::Uuid;
+
 use crate::common_utils::cache_utils::get_or_fetch_entity;
 use crate::common_utils::dao_error::DaoError;
 use crate::masters::product_item_master::product_item_dao::{get_product_item_dao, ProductItemDao};
@@ -30,6 +34,9 @@ pub trait ProductItemService: Send + Sync {
 
     async fn get_product(&self, product_id: Uuid, tenant_id: Uuid)
                          -> Result<Option<Arc<ProductItemResponse>>, ProductItemServiceError>;
+
+    async fn get_products(&self, product_ids: &[Uuid], tenant_id: Uuid)
+                          -> Result<Vec<Arc<ProductItemResponse>>, ProductItemServiceError>;
 }
 
 
@@ -74,5 +81,25 @@ impl ProductItemService for ProductItemServiceImpl {
             };
         get_or_fetch_entity(tenant_id, product_id, &self.cache_by_tenant_id_and_product_id,
                             fetch).await
+    }
+    async fn get_products(&self, product_ids: &[Uuid], tenant_id: Uuid)
+                          -> Result<Vec<Arc<ProductItemResponse>>, ProductItemServiceError> {
+        let mut list: Vec<(Uuid, Option<Arc<ProductItemResponse>>)> = Vec::with_capacity(product_ids.len());
+        for product_id in product_ids {
+            let a = self.cache_by_tenant_id_and_product_id.get(&(tenant_id, *product_id)).await;
+            list.push((*product_id, a));
+        }
+        let (cached_products, uncached_product_ids): (Vec<_>, Vec<_>) = list.into_iter().partition(|a| a.1.is_some());
+        let cached_products: Vec<Arc<ProductItemResponse>> = cached_products.into_iter().map(|a| a.1.unwrap()).collect();
+        let uncached_product_ids: Vec<Uuid> = uncached_product_ids.into_iter().map(|a| a.0).collect();
+        let db_products = self.dao.get_products(&uncached_product_ids, tenant_id).await?;
+        let mut combined_products: Vec<Arc<ProductItemResponse>> = cached_products;
+        for product in db_products {
+            let product_id = product.base_master_fields.id.clone();
+            let product_arc = Arc::new(product);
+            self.cache_by_tenant_id_and_product_id.insert((tenant_id, product_id), product_arc.clone()).await;
+            combined_products.push(product_arc);
+        }
+        Ok(combined_products)
     }
 }
