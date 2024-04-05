920eb6d18ea0a7cb7e0787da19331be274108a2a
Aayushya Vajpayee <aayushyavajpayee@gmail.com>
2024-04-05 16:42:58 +0530

    changes to cargo

--- accounting_system_rust/accounting_system/src/masters/address_master/mod.rs
+++ accounting_system_rust/accounting_system/src/masters/address_master/mod.rs
@@ -1,6 +1,6 @@
-pub mod address_model;
 mod address_dao;
-pub mod address_service;
-pub mod address_http_api;
 pub mod address_db_mapping;
+pub mod address_http_api;
+pub mod address_model;
+pub mod address_service;
 pub mod address_utils;
