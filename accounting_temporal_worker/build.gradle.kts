eeee6d629a8dca2e8af82815b4d8104e64c7837a
Aayushya Vajpayee <aayushyavajpayee@gmail.com>
2024-04-07 15:22:34 +0530

    add k2 flag and upgrade kotlin version

--- accounting_temporal_worker/build.gradle.kts
+++ accounting_temporal_worker/build.gradle.kts
@@ -3,7 +3,7 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "1.9.23"
     id("io.ktor.plugin") version "2.3.7"
     id("com.google.cloud.tools.jib") version "3.4.0"
     idea
